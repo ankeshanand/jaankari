72169dd862ed78dc1b2ba6dbe9a3bb86d687ecdb
Rohan Raja <rohanraja9@gmail.com>
2015-01-28 19:37:31 +0530

    Added File Sharing Capability

--- app/src/main/java/jaangari/opensoft/iitkgp/jaankari/SearchableActivity.java
+++ app/src/main/java/jaangari/opensoft/iitkgp/jaankari/SearchableActivity.java
@@ -1,55 +1,78 @@
 package jaangari.opensoft.iitkgp.jaankari;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import jaangari.opensoft.iitkgp.jaangari.R;
 import jaangari.opensoft.iitkgp.jaankari.hotspotUtils.CommDevice;
+import jaangari.opensoft.iitkgp.jaankari.util.Videos;
 
 
 public class SearchableActivity extends ListActivity {
-    DatabaseHandler dbHandler;
+    private DatabaseHandler dbHandler;
+    private DatabaseHandler db;
+    private final String TAG = "Searchable";
+    static final String JARGON = "Searchable";
+    private String[] videoTitle;
+    private String[] otherTitle;
+    private String[] otherSummary;
+    private Bitmap[] image;
+    private float[] rating;
+    private boolean video_set = false;
+    private int video_id;
+    private int video_history;
+
     private BroadcastReceiver receiver = new BroadcastReceiver() {
 
         @Override
         public void onReceive(Context context, Intent intent) {
             Bundle bundle = intent.getExtras();
             if (bundle != null) {
-//                String string = bundle.getString(DownloadService.FILEPATH);
-//                int resultCode = bundle.getInt(DownloadService.RESULT);
-//                if (resultCode == RESULT_OK) {
-//                    Toast.makeText(MainActivity.this,
-//                            "Download complete. Download URI: " + string,
-//                            Toast.LENGTH_LONG).show();
-//                    textView.setText("Download done");
-//                } else {
-//                    Toast.makeText(MainActivity.this, "Download failed",
-//                            Toast.LENGTH_LONG).show();
-//                    textView.setText("Download failed");
-//                }
-
                 String jsonResults = bundle.getString("JSON");
 
                 Log.d("BroadcastRec", jsonResults);
 
                 try {
-                    JSONObject jResults = new JSONObject(jsonResults);
-                    String theIP = jResults.getString("IP");
+                    final JSONObject jResults = new JSONObject(jsonResults);
+                    final String theIP = jResults.getString("IP");
                     Log.d("BroadcastRec", "IP is : " + theIP);
 
                     JSONArray results = jResults.getJSONArray("list");
@@ -57,8 +80,25 @@ public class SearchableActivity extends ListActivity {
                     for(int i=0; i< results.length(); i++ )
                     {
                         JSONObject oneCat = (JSONObject) results.get(i);
-                        Log.d("BroadcastRec", "OUTPUT : " + oneCat.toString());
+                        Log.d("BroadcastRec", "OUTPUT : " + oneCat.keys().next());
                     }
+
+                    Thread th = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            CommDevice cd = null;
+                            try {
+                                cd = new CommDevice(getApplicationContext());
+                                cd.requestFile(theIP,"",1);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    });
+
+                    th.start();
 
 
                 } catch (JSONException e) {
@@ -83,10 +123,6 @@ public class SearchableActivity extends ListActivity {
         unregisterReceiver(receiver);
     }
 
-    class PairCategory{
-        String category;
-        ArrayList<Integer> ids;
-    }
     protected void contentSearch(String Query){
 
     }
@@ -94,19 +130,19 @@ public class SearchableActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_searchable);
+//        setContentView(R.layout.activity_searchable);
         Intent intent  =  getIntent();
         dbHandler = new DatabaseHandler(getApplicationContext());
 
-        if(intent.ACTION_SEARCH.equals(intent.getAction())){
-            final String Query = intent.getStringExtra(SearchManager.QUERY);
+        if(intent.ACTION_SEARCH.equals(intent.getAction()) || true){
+            //final String Query = intent.getStringExtra(SearchManager.QUERY);
+            final String Query = "Obama";//intent.getStringExtra(SearchManager.QUERY);
             contentSearch(Query);
             Thread th = new Thread(new Runnable() {
                 @Override
                 public void run() {
                     CommDevice cd = null;
                     try {
-                        Thread.sleep(2000);
                         cd = new CommDevice(getApplicationContext());
                         cd.broadcastQuery(Query);
                     } catch (Exception e) {
@@ -139,4 +175,15 @@ public class SearchableActivity extends ListActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
+
+
+
+
+
+
+
+
+
